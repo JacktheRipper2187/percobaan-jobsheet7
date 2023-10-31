48909b4cf58ad0fa11b677b85e832447c51f2173
JacktheRipper2187 <ulilf442@gmail.com>
2023-10-31 17:40:27 +0700

    kode sebelum di modifikasi

--- DoWhileCuti22.java
+++ DoWhileCuti22.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+
 public class DoWhileCuti22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+        
+        System.out.print("Masukkan jumlah jatah cuti: ");
+        jatahCuti = sc.nextInt();
+
+        do {
+            System.out.print("Apakah ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.next();
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuci Anda tidak mencukupi");
+                    break;
+                }
+            }
+        } while (jatahCuti > 0);
+
+        
+    }
     
 }
