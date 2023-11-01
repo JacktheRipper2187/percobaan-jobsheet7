e0c858aa032140cf1fbcf3adf2a55084a54a990e
JacktheRipper2187 <ulilf442@gmail.com>
2023-11-01 15:42:28 +0700

    kelipatan menggunakan while

--- WhileKelipatan22.java
+++ WhileKelipatan22.java
@@ -4,6 +4,29 @@ public class WhileKelipatan22 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int kelipatan, jumlah, counter;
+        jumlah = 0;
+        counter = 0;
+        
+        double rata_rata = 0;
+
+        System.out.print("Masukkan bilangan kelipatan (1-9) : ");
+        kelipatan = scan.nextInt();
+        
+        int i = 1; 
+        while (i <= 50) {
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+                rata_rata = (double) jumlah / counter;
+            }
+            i++; 
+        }
+
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-Rata dari seluruh bilangan %.2f", rata_rata);
+
         
     }
 }
